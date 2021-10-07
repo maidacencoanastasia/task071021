88f191c0b5185069584e1a0007e64e80615ffc76
maidacencoanastasia <maidacenco100@gmail.com>
2021-10-07 10:01:56 +0300

    create post mapping

--- src/main/java/maidacenco/test/controller/StudentController.java
+++ src/main/java/maidacenco/test/controller/StudentController.java
@@ -1,15 +1,26 @@
 package maidacenco.test.controller;
 
+import maidacenco.test.model.Student;
 import maidacenco.test.service.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("students")
 public class StudentController {
     private StudentService studentService;
 
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
-
+// create REST API
+    @PostMapping
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student){
+        return new ResponseEntity<>(studentService.saveStudent(student), HttpStatus.CREATED);
+    }
 
 }
