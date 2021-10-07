8ea7b1d87eead967d4ed1038e9a63fe9121a8279
maidacencoanastasia <maidacenco100@gmail.com>
2021-10-07 09:56:20 +0300

    add annotation and constructor to controller

--- src/main/java/maidacenco/test/controller/StudentController.java
+++ src/main/java/maidacenco/test/controller/StudentController.java
@@ -1,4 +1,15 @@
 package maidacenco.test.controller;
 
+import maidacenco.test.service.StudentService;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class StudentController {
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
 }
