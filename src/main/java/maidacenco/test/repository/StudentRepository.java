3c852d34d96e74b5c09a97f4b93431bf4248d4e7
maidacencoanastasia <maidacenco100@gmail.com>
2021-10-07 09:38:55 +0300

    add annotation

--- src/main/java/maidacenco/test/repository/StudentRepository.java
+++ src/main/java/maidacenco/test/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package maidacenco.test.repository;
 
 import maidacenco.test.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
