a4f3aa3ab65292839024234a6d9a439da87269e9
maidacencoanastasia <maidacenco100@gmail.com>
2021-10-07 10:39:44 +0300

    upload get all students REST API

--- src/main/java/maidacenco/test/controller/StudentController.java
+++ src/main/java/maidacenco/test/controller/StudentController.java
@@ -5,9 +5,12 @@ import maidacenco.test.service.StudentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("students")
@@ -17,11 +20,17 @@ public class StudentController {
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
-// create REST API
+
+    // create REST API
     @PostMapping
-    public ResponseEntity<Student> saveStudent(@RequestBody Student student){
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student) {
         return new ResponseEntity<>(studentService.saveStudent(student), HttpStatus.CREATED);
     }
-// get REST API
+
+    // get REST API
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
 
 }
--- src/main/java/maidacenco/test/service/StudentService.java
+++ src/main/java/maidacenco/test/service/StudentService.java
@@ -6,5 +6,6 @@ import java.util.List;
 
 public interface StudentService {
     Student saveStudent(Student student);
+
     List<Student> getAllStudents();
 }
