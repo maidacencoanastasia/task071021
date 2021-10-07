d821db9a09c1ecfd73f44d910a501011e368d849
maidacencoanastasia <maidacenco100@gmail.com>
2021-10-07 08:49:41 +0300

    add annotation

--- src/main/java/maidacenco/test/model/Student.java
+++ src/main/java/maidacenco/test/model/Student.java
@@ -1,8 +1,23 @@
 package maidacenco.test.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "students")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "group", nullable = false)
     private String group;
+
+    @Column(name = "averageMark")
     private double averageMark;
 }
