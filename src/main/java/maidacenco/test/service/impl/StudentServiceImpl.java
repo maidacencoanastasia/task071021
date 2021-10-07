29e59d58a09e2a7883db2f997a79200eefafebe0
maidacencoanastasia <maidacenco100@gmail.com>
2021-10-07 09:53:50 +0300

    modify method saveStudent

--- src/main/java/maidacenco/test/service/impl/StudentServiceImpl.java
+++ src/main/java/maidacenco/test/service/impl/StudentServiceImpl.java
@@ -1,13 +1,20 @@
 package maidacenco.test.service.impl;
 
 import maidacenco.test.model.Student;
+import maidacenco.test.repository.StudentRepository;
 import maidacenco.test.service.StudentService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentServiceImpl implements StudentService {
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     @Override
     public Student saveStudent(Student student) {
-        return null;
+        return studentRepository.save(student);
     }
 }
